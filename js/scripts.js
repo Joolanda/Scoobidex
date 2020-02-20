917bb93baf161d20a0c6b7ee4415488fd669342c
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-02-20 11:09:52 +0100

    add missing syntax

--- js/scripts.js
+++ js/scripts.js
@@ -88,10 +88,10 @@ var dogRepository = (function() {
     // add the new modal content
     var $closeButtonElement = $('<button></button>');
     $closeButtonElement.html("Close" class='modal-close');
-    $closeButtonElement.on('click', function(hideModal);
+    $closeButtonElement.on('click', function(hideModal));
 
     var modalTitle = $('h1');
-    $modalTitle.html(item.message class='modal-title')
+    $modalTitle.html(item.message class='modal-title');
 
     var modalType = $('p');
     $modalType.html(item.breeds class='modal-details');
