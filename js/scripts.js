809d20de06f2924f42f87aa2213dca2b4b8a8b65
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-02-27 16:11:24 +0100

    leaving out hideModal

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,8 @@ var dogRepository = (function() {
   "use strict";
   var repository = [];
   var apiUrl = "https://dog.ceo/api/breeds/list/all";
-  var $modalContainer = $('#modal-container');
-  var $dogList = $('ul');
+  var $modalContainer = $("#modal-container");
+  var $dogList = $("ul");
 
   //Function to add new hound data
   function add(dog) {
@@ -23,14 +23,14 @@ var dogRepository = (function() {
 
   //Function to add list for each dog object
   function addListItem(dog) {
-    var $dogList = $('.dog-list');
+    var $dogList = $(".dog-list");
     var $listItem = $('<li class="list-group-item"></li>');
-    var $button = $('<button class="list-button">' + dog.name + '</button>');
+    var $button = $('<button class="list-button">' + dog.name + "</button>");
 
     $(dogList).append($listItem);
     $(listItem).append($button);
-    $(button).html(dog.name); 
-    $button.on('click', function() {
+    $(button).html(dog.name);
+    $button.on("click", function() {
       showDetails(dog);
     });
   }
@@ -48,76 +48,88 @@ var dogRepository = (function() {
   //Function to load dog list from API - in jQuery instead of fetch
   // ...return fetch(apiUrl).then(function (response)... forEach(function (item) etcetera
   function loadList() {
-    return $.ajax(apiUrl, {dataType: 'json'}).then(function(responseJSON) {
-      return responseJSON;
-    }).then(function(json) {
-      json.results.forEach(function(item) {
+    return $.ajax(apiUrl, { dataType: "json" })
+      .then(function(item) {
+        $.each(item.results, function(index, item) {
           var dog = {
             breeds: item.message,
             detailsUrl: item.url
           };
           add(dog);
         });
-      }).catch(function(e) {
+      })
+      .catch(function(e) {
         console.error(e);
       });
   }
 
   function loadDetails(item) {
     var url = item.detailsUrl;
-    return $.ajax(url, {dataType: 'json'}).then(function(responseJSON) {
-      return responseJSON;
-    }).then(function(details) {
-        // add the details to the item
+    return $.ajax(url, { dataType: "json" })
+      .then(function(details) {
+        // Now we add the details to the item
         item.imageUrl = details.sprites.front_default;
-                                item.types = details.subbreeds;
-        item.types = details.types.map(function(dog){
-        return dog.subbreed.name;
-
-      }).catch(function(e) {
+        item.types = Object.keys(details.types);
+      })
+      .catch(function(e) {
         console.error(e);
       });
-    });
+  }
+
+  function loadDetails(item) {
+    var url = item.detailsUrl;
+    return $.ajax(url, { dataType: "json" })
+      .then(function(responseJSON) {
+        return responseJSON;
+      })
+      .then(function(details) {
+        // add the details to the item
+        item.imageUrl = details.sprites.front_default;
+        item.types = details.subbreeds;
+        item.types = details.types
+          .map(function(dog) {
+            return dog.subbreed.name;
+          })
+          .catch(function(e) {
+            console.error(e);
+          });
+      });
   }
 
   // Function to show a modal with title and text
 
   function showModal(item) {
     //clear all existing modal content
-    $modalContainer.html('');
-    $modalContainer.addClass('is-visible');
+    $modalContainer.empty();
+    $modalContainer.addClass("is-visible");
 
     var $modal = $('<div class="modal"></div>');
 
     // add the new modal content
-    var $closeButtonElement = $('<button type="button" class="modal-close">Close</button>');
-    $closeButtonElement.on('click', function (hideModal) {
-      hideModal()
-    });
+    var closeButtonElement = $('<button class="modal-close">Close</button>');
+    closeButtonElement.on("click", hideModal);
 
-    var modalTitle = $('h1');
-    $modalTitle.html(item.name class='modal');
+    var modalTitle = $("<h1>" + item.message + "</h1>");
 
-    var modalType = $('p');
-    $modalType.html('sub-breed: ' + item.types class='modal');
+    var modalType = $("<p>" + "Type : " + item.breeds + "</p>");
 
     //dogbreed display image in modal
-    var imageElement = $('img');
-    $imageElement.html(item.imageUrl class='modal-img');
+    var imageElement = $('<img class="modal-img">');
+    imageElement.attr("src", item.imageUrl);
 
-    $modal.append($closeButtonElement);
-    $modal.append($imageElement);
-    $modal.append($modalTitle);
-    $modal.append($modalType);
+    $modal.append(closeButtonElement);
+    $modal.append(imageElement);
+    $modal.append(modalTitle);
+    $modal.append(modalType);
     $modalContainer.append($modal);
   }
 
-  function hideModal() {
-    var $modalContainer = $('#modal-container' removeClass('is-visible'));
-  }
-    var $modalContainer.on('click', function(showModal) => {
-    ('Modal title', "This is the modal content!");
-    });
+  //function hideModal() {
+  // var $modalContainer = $('#modal-container' removeClass('is-visible'));
+  // }
+  //   var $modalContainer.on('click', function(showModal) => {
+  //   ('Modal title', "This is the modal content!");
+  //   });
 
   //Function to show details of each dog breed
   function showDetails(item) {
@@ -134,8 +146,8 @@ var dogRepository = (function() {
     addListItem: addListItem,
     loadList: loadList,
     loadDetails: loadDetails,
-    showModal: showModal,
-    hideModal: hideModal
+    showModal: showModal
+    //hideModal: hideModal
   };
 })();
 // END of IIFE for Scoobidex repository
